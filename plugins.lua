039092b4e57142d6aaa7cab4ef2a048f22806fbc
Amree Zaid <mohd.amree@gmail.com>
2023-12-07 21:59:02 +0800

    Update with more plugins

--- plugins.lua
+++ plugins.lua
@@ -33,8 +33,56 @@ local plugins = {
     lazy = false,
     keys = {"sa", "sd", "sr"},
     event = "BufRead",
-  }
+  },
+  -- open code on the web
+  {
+    "linrongbin16/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup()
+    end,
+  },
+  -- strip trailing whitespace
+  {
+    "echasnovski/mini.trailspace",
+    version = false,
+    event = "BufRead",
+    config = function ()
+      require("mini.trailspace").setup()
 
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        command = "lua MiniTrailspace.trim()"
+      })
+    end
+  },
+  -- git
+  {
+    "tpope/vim-fugitive",
+    event = "BufRead",
+  },
+  -- end block support
+  {
+    "RRethy/nvim-treesitter-endwise",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        endwise = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require "custom.configs.copilot"
+    end,
+  }
 }
 
 return plugins
+
