2e785b0edae0fc564ca68ed783441885f0648816
Amree Zaid <mohd.amree@gmail.com>
2024-09-14 02:02:14 +0800

    Added noice and nvim-notify

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -68,6 +68,37 @@ local plugins = {
 			vim.g.matchup_matchparen_offscreen = { method = "popup" }
 		end,
 	},
+
+	-- cool notifier
+	{
+		"rcarriga/nvim-notify",
+		event = "BufRead",
+		config = function()
+			vim.notify = require("notify")
+		end,
+	},
+
+	-- noice
+	{
+		"folke/noice.nvim",
+		event = "BufRead",
+		config = function()
+			require("noice").setup({
+				lsp = {
+					signature = {
+						enabled = false, -- Add this line to disable signature help
+					},
+				},
+				-- you can enable a preset for easier configuration
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					lsp_doc_border = false, -- add a border to hover docs and signature help
+				},
+			})
+		end,
+	},
 }
 
 return plugins
