b4d153b2a8625a58a08f185511931dbd0b07f95b
Amree Zaid <mohd.amree@gmail.com>
2024-12-08 13:33:53 +0800

    Update

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,30 +1,30 @@
 local configs = require("nvchad.configs.lspconfig")
 local util = require("lspconfig.util")
 
--- local function is_ruby_version_3_or_above()
--- 	local ruby_version_path = vim.fn.getcwd() .. "/.ruby-version"
--- 	if vim.fn.filereadable(ruby_version_path) == 0 then
--- 		return false
--- 	end
---
--- 	local file = io.open(ruby_version_path, "r")
--- 	if not file then
--- 		return false
--- 	end
---
--- 	local version = file:read("*line")
--- 	file:close()
---
--- 	if version then
--- 		-- Remove any prefix (like 'ruby-' or 'jruby-')
--- 		version = version:gsub("^%w+%-", "")
--- 		-- Extract the major version number
--- 		local major_version = tonumber(version:match("^(%d+)"))
--- 		return major_version and major_version >= 3
--- 	end
---
--- 	return false
--- end
+local function is_ruby_version_3_or_above()
+	local ruby_version_path = vim.fn.getcwd() .. "/.ruby-version"
+	if vim.fn.filereadable(ruby_version_path) == 0 then
+		return false
+	end
+
+	local file = io.open(ruby_version_path, "r")
+	if not file then
+		return false
+	end
+
+	local version = file:read("*line")
+	file:close()
+
+	if version then
+		-- Remove any prefix (like 'ruby-' or 'jruby-')
+		version = version:gsub("^%w+%-", "")
+		-- Extract the major version number
+		local major_version = tonumber(version:match("^(%d+)"))
+		return major_version and major_version >= 3
+	end
+
+	return false
+end
 
 local servers = {
 	cssls = {},
@@ -39,16 +39,17 @@ local servers = {
 				analyses = {
 					unusedparams = true,
 				},
+				gofumpt = true,
 			},
 		},
 	},
 	html = {},
 	-- rubocop = {},
-	-- ruby_lsp = {
-	-- 	-- TODO: Figure out how to do itwithout executing a shell command
-	-- 	-- zsh -c "source ~/.zshrc && chruby $(cat .ruby-version) && ruby-lsp"
-	-- 	cmd = { "/usr/local/bin/rubylsp" },
-	-- },
+	ruby_lsp = {
+		-- TODO: Figure out how to do itwithout executing a shell command
+		-- zsh -c "source ~/.zshrc && chruby $(cat .ruby-version) && ruby-lsp"
+		-- cmd = { "/usr/local/bin/rubylsp" },
+	},
 	-- standardrb = {},
 	ts_ls = {},
 	-- taplo = {},
